81c74386b1fffa60c14add33f3686552a367d885
1stimoney <daudasheriff020@gmail.com>
2025-08-23 13:02:49 -0700

    add postgress and drizzle

---
+++ src/db/drizzle.ts
@@ -0,0 +1,10 @@
+import * as schema from './schema'
+import { neon } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-http'
+
+const sql = neon(process.env.POSTGRES_URL!)
+
+const db = drizzle(sql, {
+  schema,
+})
+export default db
