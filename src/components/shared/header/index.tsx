935fefc21aad83640584426be87bf50baf62282c
1stimoney <daudasheriff020@gmail.com>
2025-08-24 14:04:57 -0700

    user auth

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { APP_NAME } from '@/lib/constants'
-import { Button } from '@/components/ui/button'
-import { MenuIcon, ShoppingCart, UserIcon } from 'lucide-react'
+import Menu from './menu'
 
 const Header = async () => {
   return (
@@ -19,20 +18,7 @@ const Header = async () => {
             {APP_NAME}
           </Link>
         </div>
-        <div className='space-x-2'>
-          <Button asChild variant={'ghost'}>
-            <Link href='/cart'>
-              <ShoppingCart />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href={'/sign-in'}>
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   )
